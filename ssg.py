f57fb472f3dc70896e3fe083f8693738beef595c
joeboulter <joe.boulter@gmail.com>
2021-04-15 09:33:49 +0100

    Corrections

--- ssg.py
+++ ssg.py
@@ -1,9 +1,13 @@
 import typer
+
 from ssg.site import Site
+
 import ssg.parsers
 
 def main(source = "content", dest = "dist"):
-    config = {'source': source, 'dest': dest, 'parsers': [ssg.parsers.ResourceParser()]}
+    config = {'source': source,
+    'dest': dest,
+    'parsers': [ssg.parsers.ResourceParser()]}
     site = Site(**config).build()
 
 typer.run(main)
